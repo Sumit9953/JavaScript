1d4d1677c52a12a133262b7a2383edccd78b8f8e
Santosh kumar <sumitsantosh555@gmail.com>
2022-09-17 17:05:20 +0530

    Compelet Basic Challange

---
+++ Basic chalange/app.js
@@ -0,0 +1,58 @@
+/*function Sum(num1,num2){
+    return num1+num2;
+}
+
+console.log(Sum(-3,9)); 
+
+//------------------------convert hours into sec-------------------
+
+function HoursintoSec(hours){
+    return hours*3600;
+}
+
+console.log(HoursintoSec(10));
+
+//--------------------------calculate perimeter of reactangle----------------
+
+function perimeter(length,width){
+    return 2*(length+width);
+}
+
+console.log(perimeter(20,10));
+
+//------------------calculate area of triangle-------------------
+
+function AreaofTriangle(base,height){
+    return 0.5*base*height;
+}
+
+console.log(AreaofTriangle(10,10));
+
+//----------------------------Extend a string---------------------
+
+function Addstring(str){
+    return str+"Frontend";
+}
+
+console.log(Addstring("Apple"));
+
+//-----------------------------Greater than 100--------------------------------
+
+function Greater(num1,num2){
+
+    return num1+num2 > 100;
+    // let sum = num1+num2;
+    // if(sum > 100){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+}
+
+console.log(Greater(40,60));*/
+
+function oppositebool(bool){
+    return !bool;
+}
+
+console.log(oppositebool(false));
